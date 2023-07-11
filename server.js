ae3f4b262ac0ed56b18af802ebacba4820e19555
Shuvo Sarker <sarker3664@gmail.com>
2023-07-11 13:09:32 +0600

    docs - mongodb connected using mongoose

--- server.js
+++ server.js
@@ -2,9 +2,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import connectDB from './config/db.js';
 
 // Dot ENV config
 dotenv.config();
+
+// mongodb connection
+connectDB();
 
 // rest object
 const app = express();
@@ -20,7 +24,7 @@ const PORT = process.env.PORT || 8080;
 // listen
 app.listen(PORT, () => {
     console.log(
-        `node server running on ${process.env.DEV_MODE.toUpperCase()} mode with port ${PORT}`
+        ` node server running on ${process.env.DEV_MODE.toUpperCase()} mode with port ${PORT} `
             .bgGreen.white
     );
 });
