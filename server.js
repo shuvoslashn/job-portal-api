dc8f67660924b1e51ba150479735d0c711252441
Shuvo Sarker <sarker3664@gmail.com>
2023-07-11 09:57:30 +0600

    docs - server created

---
+++ server.js
@@ -0,0 +1,26 @@
+// imports
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+
+// Dot ENV config
+dotenv.config();
+
+// rest object
+const app = express();
+
+// routes
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to Job Portal</h1>');
+});
+
+// port
+const PORT = process.env.PORT || 8080;
+
+// listen
+app.listen(PORT, () => {
+    console.log(
+        `node server running on ${process.env.DEV_MODE.toUpperCase()} mode with port ${PORT}`
+            .bgGreen.white
+    );
+});
